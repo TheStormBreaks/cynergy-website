da4bfb455ed3159a772d015eff941a9b413bc9f8
Somya Jha <sumicode.exe@gmail.com>
2025-08-20 06:53:43 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,17 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCSyjUsDUI7UnEWEI_2cOtKZ1BvpCq1z4Q",
   authDomain: "cynergy-website-75db7.firebaseapp.com",
   projectId: "cynergy-website-75db7",
-  storageBucket: "cynergy-website-75db7.appspot.com",
+  storageBucket: "cynergy-website-75db7.firebasestorage.app",
   messagingSenderId: "69980776805",
   appId: "1:69980776805:web:db747823f07753dc07d98c",
   measurementId: "G-KN6RCQBGZE"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
